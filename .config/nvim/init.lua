a02a0522614077cce8e279abd097d85e31685072
jmgonzalezro <jmgonzalezro@gmail.com>
2024-02-04 18:39:34 +0100

    added git blame inline

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -76,6 +76,16 @@ require('lazy').setup({
     'lewis6991/gitsigns.nvim',
     opts = {
       -- See `:help gitsigns.txt`
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+      },
+      current_line_blame_formatter_opts = {
+        relative_time = false,
+      },
       signs = {
         add = { text = '+' },
         change = { text = '~' },
