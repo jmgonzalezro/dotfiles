b57e2c22825d9d157e463828ca7b83534a5645e6
jmgonzalez <jmgonzalezro@gmail.com>
2024-05-15 22:46:05 +0200

    test to delete

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,8 +5,6 @@ vim.g.maplocalleader = ' '
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
     'git',
     'clone',
